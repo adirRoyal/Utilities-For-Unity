df538478b1d282562a1ad6b8c749a3b1f03cf63b
adirRoyal <119738846+adirRoyal@users.noreply.github.com>
2024-12-26 12:46:12 +0200

    Create PlayerPrefsUtility.cs
    Manage saved data such as points, statistics, etc.

---
+++ PlayerPrefsUtility.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public static class PlayerPrefsUtility
+{
+    public static void SaveInt(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+        PlayerPrefs.Save();
+    }
+
+    public static int LoadInt(string key, int defaultValue = 0)
+    {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetInt(key) : defaultValue;
+    }
+
+    public static void SaveFloat(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
+    }
+
+    public static float LoadFloat(string key, float defaultValue = 0f)
+    {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetFloat(key) : defaultValue;
+    }
+
+    public static void SaveString(string key, string value)
+    {
+        PlayerPrefs.SetString(key, value);
+        PlayerPrefs.Save();
+    }
+
+    public static string LoadString(string key, string defaultValue = "")
+    {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetString(key) : defaultValue;
+    }
+}
